b9a302aa218d3ccad3e91c3ddf2c03ba3a59dba7
Katy Felkner <katy.felkner@ou.edu>
2018-12-01 12:13:30 -0600

    add debug code

--- checkAvail.js
+++ checkAvail.js
@@ -2,6 +2,7 @@ function checkAvail() {
 
     // get entry from that field
     var gid = document.getElementById("groupID").value;
+    console.log("value from text box is: " + gid);
 
     if (gid == "group ID") {
         // set "pls enter value" message visible
