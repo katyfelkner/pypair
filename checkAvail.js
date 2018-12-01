6353b5343de75faff23091d6450e6dde82f03c79
Katy Felkner <katy.felkner@ou.edu>
2018-12-01 12:05:17 -0600

    fix typo in checkAvail.js

--- checkAvail.js
+++ checkAvail.js
@@ -1,24 +1,13 @@
 function checkAvail() {
 
     // get entry from that field
-    var gid = document.getElementbyID("groupID").value;
+    var gid = document.getElementByID("groupID").value;
 
     if (gid == "group ID") {
         // set "pls enter value" message visible
-        document.getElementbyID.display = "block";
+        document.getElementByID.display = "block";
     }
 
-    // first step: get the class of a moused over element (and highlight that element)
-    var classes = this.getAttribute('class').split(" ");
 
-    // second step: find all other elements, in all other languages, that match that class
-    for (i = 0; i < classes.length; i++) {
-        var same = document.getElementsByClassName(classes[i])
-        for (j = 0; j < same.length; j++) {
-            //third step: highlight them!
-            same[j].style.backgroundColor='pink';
-        }
-
-    }
 
 }
