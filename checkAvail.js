346f9c7ab28e49db455ef61174f9b1919eeb1cc3
Katy Felkner <katy.felkner@ou.edu>
2018-12-01 15:17:49 -0600

    add updated js

--- checkAvail.js
+++ checkAvail.js
@@ -1,13 +1,18 @@
 function checkAvail() {
-
+    console.log("in the function");
     // get entry from that field
     var gid = document.getElementById("groupID").value;
     console.log("value from text box is: " + gid);
 
     if (gid == "group ID") {
+    console.log("in the loop");
         // set "pls enter value" message visible
-        document.getElementById("EnterValueError").display = "block";
+        document.getElementById("EnterValueError").style.display = "block";
     }
+
+    // at this point, call a server side function to check availability
+
+
 
 
 
